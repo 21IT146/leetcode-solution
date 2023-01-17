2b18facf72fa1d2897c5754dd1f94f8047e3f544
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-01-17 15:28:54 +0530

    Create 1431. Kids With the Greatest Number of Candies Solution.java

---
+++ 1431. Kids With the Greatest Number of Candies Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> list=new ArrayList<>();
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<candies.length;i++)
+        {
+            if(candies[i]>max)
+            {
+                max=candies[i];
+            }
+        }
+        for(int i:candies)
+        {
+            if((extraCandies+i)>=max)
+            {
+                list.add(true);
+            }
+            else
+            {
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
