b6e1667fa7b4ad83bd3eb6413ea40cd08f281b5c
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-01-22 23:56:17 +0530

    Create 1539. Kth Missing Positive Number Solution.java

---
+++ 1539. Kth Missing Positive Number Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int computermssingcount(int actual, int expected){
+        return actual-expected;
+    }
+    public static int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int missingcount = computermssingcount(arr[n-1], n);
+        int left = 0, right = n-1;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            missingcount = computermssingcount(arr[mid],mid+1);
+            if(missingcount >= k) right = mid-1;
+            else left = mid+1;
+        }
+        if(right == -1) return k;
+        return arr[right] + k - computermssingcount(arr[right],right+1);
+    }
+}
