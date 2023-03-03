85ace4cb6d4a555ab04c0520f857dec8ae570994
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-03-03 21:36:22 +0530

    Create 1389. Create Target Array in the Given Order Solution.java

---
+++ 1389. Create Target Array in the Given Order Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[nums.length];
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            arr.add(index[i],nums[i]);
+            
+        }
+        for(int i=0;i<arr.size();i++){
+            target[i] = arr.get(i);
+        }
+        return target;
+    }
+}
