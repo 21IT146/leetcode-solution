c9894ed587385c870962a02df5fc60d7ea486fa3
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-03-27 21:40:37 +0530

    Create 1534. Count Good Triplets Solution.java

---
+++ 1534. Count Good Triplets Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int x = 0, size = arr.length;
+        for(int i = 0; i <size - 2; i++){
+            for(int j = i + 1; j < size - 1; j++){
+                if(Math.abs(arr[i] - arr[j]) <= a){
+                for(int k = j + 1; k < size; k++){
+                if(Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c)
+                    x++;
+                }
+                }
+            }
+        }
+        return x;
+    }
+}
