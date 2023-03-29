76e959113508016f087822a97bed77879c4d3724
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-03-29 22:13:26 +0530

    Create 1282. Group the People Given the Group Size They Belong To Solution.java

---
+++ 1282. Group the People Given the Group Size They Belong To Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> ans = new ArrayList<>();   
+        for (int i = 0; i < groupSizes.length; i++) {
+            
+            int size = groupSizes[i];   
+            map.putIfAbsent(size, new ArrayList<>());
+          
+            if (map.get(size).size() == size) {
+                ans.add(map.get(size));  
+                map.put(size, new ArrayList<>()); 
+            }
+
+            map.get(size).add(i);
+        }
+        ans.addAll(map.values());  
+        return ans;   
+    }
+}
