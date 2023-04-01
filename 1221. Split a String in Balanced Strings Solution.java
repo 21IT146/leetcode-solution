dd3900ecd3d59690773a27bf3f2c71d39c88a717
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-04-01 20:34:06 +0530

    Update 1221. Split a String in Balanced Strings Solution.java

--- 1221. Split a String in Balanced Strings Solution.java
+++ 1221. Split a String in Balanced Strings Solution.java
@@ -2,7 +2,6 @@ class Solution {
     public int balancedStringSplit(String s) 
     {
         int ans =0, sum =0;
-        
         for(int i=0; i<s.length();i++)
         {
             char ch = s.charAt(i);
