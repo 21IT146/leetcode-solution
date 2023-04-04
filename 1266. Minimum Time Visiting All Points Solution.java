4a639e6540c865963093af323cd5129201507693
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-04-04 21:51:34 +0530

    Update 1266. Minimum Time Visiting All Points Solution.java

--- 1266. Minimum Time Visiting All Points Solution.java
+++ 1266. Minimum Time Visiting All Points Solution.java
@@ -3,8 +3,8 @@ class Solution {
     int ans = 0;
 
     for (int i = 1; i < points.length; ++i)
-      ans += Math.max(Math.abs(points[i][0] - points[i - 1][0]),
-                      Math.abs(points[i][1] - points[i - 1][1]));
+      ans += Math.max(Math.abs(points[i][0] - points[i - 1][0])
+             Math.abs(points[i][1] - points[i - 1][1]));
 
     return ans;
   }
