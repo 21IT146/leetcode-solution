9239cf0fd7c7ef6f42f717593502b23664df8066
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-04-14 14:43:13 +0530

    Update 1108. Defanging an IP Address Solution.java

--- 1108. Defanging an IP Address Solution.java
+++ 1108. Defanging an IP Address Solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public String defangIPaddr(String address) {
         
-        return address.replace(".","[.]");
-        
+        return address.replace(".","[.]"); 
     }
 }
