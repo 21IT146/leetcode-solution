4d1a453a733e0f32b85f2025aaf2c0b67daf9af3
Nasir Sherasiya <97278583+21IT146@users.noreply.github.com>
2023-05-07 23:41:28 +0530

    Create 112. Path Sum Solution.java

---
+++ 112. Path Sum Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public boolean hasPathSum(TreeNode root, int targetSum){
+        path(root);
+        return has(root,targetSum);
+    }
+    public boolean has(TreeNode root, int targetSum) {
+        if(root==null)
+        return false;
+
+        if(root.left==null && root.right==null){
+            if(root.val==targetSum)
+            return true;
+        }
+
+        return has(root.left,targetSum) || has(root.right,targetSum);
+    }
+
+    public static void path(TreeNode root){
+        if(root==null)
+        return;
+        int data = root.val;
+        if(root.left!=null)
+        root.left.val += data;
+
+        if(root.right!=null)
+        root.right.val += data;
+
+        path(root.left);
+        path(root.right);
+    }
+}
